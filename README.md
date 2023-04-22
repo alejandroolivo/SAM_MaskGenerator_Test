1c7c0e1dc165296a5d93c0874c70d0892ea39770
BCNVISION\alejandro.olivo <alejandro.olivo@bcnvisiongroup.com>
2023-04-22 13:46:58 +0200

    first commit - mask generator

---
+++ README.md
@@ -0,0 +1,27 @@
+# SegmentAnythingModel_first_tests
+
+Este script de Python utiliza la librería SAM de Facebook para segmentar una imagen. SAM (Segment Anything with Masking) es una librería de código abierto que permite la segmentación de objetos en imágenes de forma automática. En este script se utiliza SAM para generar máscaras que identifican objetos en una imagen.
+
+# Requerimientos:
+
+matplotlib==3.7.1
+numpy==1.24.2
+opencv_python==4.7.0.72
+segment_anything==1.0
+
+Descargar los checkpoints de modelo de la página de GitHub de segment-anything:
+
+https://github.com/facebookresearch/segment-anything#model-checkpoints
+
+# Instrucciones de uso:
+
+Colocar la imagen que se desea segmentar en la carpeta "image_examples" dentro del directorio donde se encuentra el script.
+
+Establecer los parámetros deseados:
+- model_size: Tamaño del modelo SAM que se desea utilizar (small, medium o large).
+- device: Dispositivo donde se ejecutará el modelo (cuda:0 para GPU o cpu para CPU).
+- image_name: Nombre de la imagen que se desea segmentar.
+
+Ejecutar el script.
+
+El script mostrará la imagen original junto con las máscaras que identifican los objetos en la imagen. También se puede descomentar la sección de código que muestra las máscaras por separado. El tiempo de ejecución se mostrará en la consola.
---
+++ SAM_MaskGenerator_example.py
@@ -0,0 +1,86 @@
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+import cv2
+import sys
+import io
+from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
+
+# params
+model_size = "small" # small, medium, large
+device = "cuda:0" # cuda:0, cpu
+image_name = "dog.png"
+
+# load image
+image = cv2.imread('image_examples/' + image_name)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+sys.path.append("..")
+
+# download model checkpoint (https://github.com/facebookresearch/segment-anything#model-checkpoints)
+if(model_size == "small"):
+    sam_checkpoint = "model_checkpoint/sam_vit_b_01ec64.pth"
+    model_type = "vit_b"
+
+if(model_size == "medium"):
+    sam_checkpoint = "model_checkpoint/sam_vit_l_0b3195.pth"
+    model_type = "vit_l"
+
+if(model_size == "large"):
+    sam_checkpoint = "model_checkpoint/am_vit_h_4b8939.pth"
+    model_type = "vit_h"
+
+# load model and send to device
+sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+sam.to(device=device)
+
+#load mask generator
+mask_generator = SamAutomaticMaskGenerator(sam)
+
+# get time
+start = time.time()
+
+masks = mask_generator.generate(image)
+
+# get time
+end = time.time()
+print('Elapsed time = ' + str((end - start)*1000) + ' ms')
+
+#plot image with all masks overlayed
+def show_anns(anns):
+    if len(anns) == 0:
+        return
+    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
+    ax = plt.gca()
+    ax.set_autoscale_on(False)
+    polygons = []
+    color = []
+    for ann in sorted_anns:
+        m = ann['segmentation']
+        img = np.ones((m.shape[0], m.shape[1], 3))
+        color_mask = np.random.random((1, 3)).tolist()[0]
+        for i in range(3):
+            img[:,:,i] = color_mask[i]
+        ax.imshow(np.dstack((img, m*0.35)))
+
+plt.figure(figsize=(10,10))
+plt.imshow(image)
+show_anns(masks)
+plt.axis('off')
+plt.show() 
+
+#plot first mask
+# plt.figure(figsize=(10,10))
+# plt.imshow(image)
+# plt.imshow(masks[0]['segmentation'], alpha=0.5)
+# plt.axis('off')
+# plt.show()
+
+# plot all masks
+# plt.figure(figsize=(10,10))
+# plt.imshow(image)
+# for mask in masks:
+#     plt.imshow(mask['segmentation'], alpha=0.5)
+# plt.axis('off')
+# plt.show()
+
